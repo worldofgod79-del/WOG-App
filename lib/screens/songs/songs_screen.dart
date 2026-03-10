577b10aaf25099912d200ae19e66fcbb278fb20e
worldofgod79-del <worldofgod79@gmail.com>
2026-03-10 22:50:24 +0530

    Create songs_screen.dart

---
+++ lib/screens/songs/songs_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SongsScreen extends StatelessWidget {
+  const SongsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Songs Section",
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
