5eb27eedfe05feda50a47a79d59b75a8395d8767
worldofgod79-del <worldofgod79@gmail.com>
2026-03-10 22:51:18 +0530

    Create live_screen.dart

---
+++ lib/screens/live/live_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LiveScreen extends StatelessWidget {
+  const LiveScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Live Section",
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
