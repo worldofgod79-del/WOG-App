cd7dbe031b5faa5d760a137fcd04d4280be93546
worldofgod79-del <worldofgod79@gmail.com>
2026-03-10 22:50:50 +0530

    Create books_screen.dart

---
+++ lib/screens/books/books_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BooksScreen extends StatelessWidget {
+  const BooksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Books Section",
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
