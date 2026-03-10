952d2dff614f240d4893886988a4018bb06e4393
worldofgod79-del <worldofgod79@gmail.com>
2026-03-10 22:49:56 +0530

    Create bible_screen.dart

---
+++ lib/screens/bible/bible_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BibleScreen extends StatelessWidget {
+  const BibleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Bible Section",
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
