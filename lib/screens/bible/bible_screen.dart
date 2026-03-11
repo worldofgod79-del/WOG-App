bb7c49541cebb1a6654e624eac52a22e988d4a2b
worldofgod79-del <worldofgod79@gmail.com>
2026-03-11 06:41:55 +0530

    Update bible_screen.dart

--- lib/screens/bible/bible_screen.dart
+++ lib/screens/bible/bible_screen.dart
@@ -1,15 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+import '../../models/verse.dart';
 
-class BibleScreen extends StatelessWidget {
-  const BibleScreen({super.key});
+class ChapterScreen extends StatefulWidget {
+
+  final List<Verse> bible;
+  final String book;
+  final int chapter;
+  final double fontSize;
+
+  const ChapterScreen({
+    super.key,
+    required this.bible,
+    required this.book,
+    required this.chapter,
+    required this.fontSize,
+  });
+
+  @override
+  State<ChapterScreen> createState() => _ChapterScreenState();
+}
+
+class _ChapterScreenState extends State<ChapterScreen> {
+
+  Set<int> selected = {};
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Bible Section",
-        style: TextStyle(fontSize: 24),
-      ),
+
+    final verses = widget.bible.where((v) =>
+    v.book == widget.book && v.chapter == widget.chapter).toList();
+
+    return ListView.builder(
+
+      itemCount: verses.length,
+
+      itemBuilder: (context, i) {
+
+        final verse = verses[i];
+
+        bool isSelected = selected.contains(i);
+
+        return GestureDetector(
+
+          onLongPress: () {
+
+            setState(() {
+              selected.add(i);
+            });
+
+          },
+
+          onTap: () {
+
+            if (selected.isNotEmpty) {
+
+              setState(() {
+
+                if (selected.contains(i)) {
+                  selected.remove(i);
+                } else {
+                  selected.add(i);
+                }
+
+              });
+
+            }
+
+          },
+
+          child: Container(
+
+            padding: const EdgeInsets.all(12),
+
+            color: isSelected ? Colors.yellow.shade200 : null,
+
+            child: RichText(
+
+              text: TextSpan(
+
+                style: TextStyle(
+                  fontSize: widget.fontSize,
+                  color: Colors.black,
+                ),
+
+                children: [
+
+                  TextSpan(
+                    text: "${verse.verse} ",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+
+                  TextSpan(text: verse.text)
+
+                ],
+
+              ),
+
+            ),
+
+          ),
+
+        );
+
+      },
+
     );
+
   }
+
 }
