518dabc285b4e74bcf52963ac5d4cb0d5462768c
worldofgod79-del <worldofgod79@gmail.com>
2026-03-11 06:39:15 +0530

    Update verse.dart

--- lib/models/verse.dart
+++ lib/models/verse.dart
@@ -1,1 +1,15 @@
+class Verse {
 
+  final String book;
+  final int chapter;
+  final int verse;
+  final String text;
+
+  Verse({
+    required this.book,
+    required this.chapter,
+    required this.verse,
+    required this.text,
+  });
+
+}
